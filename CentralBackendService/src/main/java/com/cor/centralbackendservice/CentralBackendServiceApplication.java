5eaa95af247d5fb006b27a5653975a5ba988a3bc
LXII <rolerlozz@gmail.com>
2025-09-16 18:50:35 +0300

    Initial commit

---
+++ CentralBackendService/src/main/java/com/cor/centralbackendservice/CentralBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cor.centralbackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralBackendServiceApplication.class, args);
+    }
+
+}
